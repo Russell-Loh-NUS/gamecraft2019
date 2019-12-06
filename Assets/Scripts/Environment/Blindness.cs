e8467f0f0877ad1be337c32dab6ed3aba6fba1f4
Russell Loh <roys000@live.com>
2019-12-06 18:15:03 +0800

    Add flickering indicator for blindness

--- Assets/Scripts/Environment/Blindness.cs
+++ Assets/Scripts/Environment/Blindness.cs
@@ -5,14 +5,18 @@ using UnityEngine;
 public class Blindness : MonoBehaviour
 {
     public GameObject directionalLight;
+    public float blinkRate = 0.1f;
 
     private float startBlindnessTimer;
     private float blindnessTimer;
+    private float blinkTimer;
     private bool isBlind = false;
+    private bool shouldBlink = false;
 
     // Start is called before the first frame update
     void Start()
     {
+        blinkTimer = blinkRate;
         RandomStartBlindnessTimer();
         RandomBlindnessTimer();
     }
@@ -20,7 +24,13 @@ public class Blindness : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (startBlindnessTimer <= 0.0f && !isBlind)
+        if(startBlindnessTimer > 0.0f && startBlindnessTimer <= 2.0f && !isBlind) {
+            if (blinkTimer <= 0.0f) {
+                blinkTimer = Random.Range(blinkRate, blinkRate + 0.3f);
+                directionalLight.SetActive(!directionalLight.activeSelf);
+            }
+        }
+        else if (startBlindnessTimer <= 0.0f && !isBlind)
         {
             SetBlind(true);
             RandomBlindnessTimer();
@@ -39,11 +49,12 @@ public class Blindness : MonoBehaviour
     {
         startBlindnessTimer -= Time.deltaTime;
         blindnessTimer -= Time.deltaTime;
+        blinkTimer -= Time.deltaTime;
     }
 
     private void RandomStartBlindnessTimer()
     {
-        startBlindnessTimer = Random.Range(15.0f, 22.0f);
+        startBlindnessTimer = Random.Range(15.0f, 30.0f);
     }
 
     private void RandomBlindnessTimer()
